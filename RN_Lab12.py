bbbd67559fdb02bfaccb34e7b33dd8ecd6a95989
rahulnimmagaddasai <rahul.nimmagadda@ufl.edu>
2026-04-17 13:55:39 -0400

    Patterns Commit

--- RN_Lab12.py
+++ RN_Lab12.py
@@ -1,12 +1,14 @@
 from patterns import *
 
+
 class Pattern:
-  def __init__(self, data):
-    self.data = data
-  def make_image(self, filename):
-      height = len(self.data)
-      width = len(self.data[0])
-      with open(filename, 'w') as f:
-          f.write(f"P2 {width} {height} 255\n")
-          for row in self.data:
-              f.write(" ".join(str(val) for val in row) + " \n")
+    def __init__(self, data):
+        self.data = data
+
+    def make_image(self, filename):
+        height = len(self.data)
+        width = len(self.data[0])
+        with open(filename, 'w') as f:
+            f.write(f"P2 {width} {height} 255\n")
+            for row in self.data:
+                f.write(" ".join(str(val) for val in row) + " \n")
